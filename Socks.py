76d7dc9aeb89ff013506567931914d2791ff939d
max <max@treesfromspace.com>
2020-01-05 00:21:59 -0500

    static methods don't change fields in their class

--- Socks.py
+++ Socks.py
@@ -6,6 +6,7 @@ class Socks:
         self.brand = brand
         self.style = style
 
+    @staticmethod
     def are_soft(fabric):
         types_of_fabric = ['wool', 'cotton', 'cashmere']
         if fabric in types_of_fabric:
@@ -13,7 +14,15 @@ class Socks:
         else:
             return print('your socks are NOT soft')
 
+    @staticmethod
+    def number_in_drawer(drawer):
+        sock_count = 0
+        for _ in drawer:
+            sock_count += 1
+        return print(f'you have {sock_count} socks in your drawer')
+
 
 my_sock = Socks('red', 10, 'polyester', 'Nike', 'tube')
 print(vars(my_sock))
 Socks.are_soft('polyester')
+Socks.number_in_drawer(['tube', 'anklet', 'quarter'])
